3bdb38efb7ec6fd402947546b7a955539094af49
Keller <381796751@qq.com>
2020-04-21 23:51:12 +0800

    路由异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,95 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/pages/Index/template.vue'
-import Create from '@/pages/Create/template.vue'
-import Detail from '@/pages/Detail/template.vue'
-import Edit from '@/pages/Edit/template.vue'
-import Login from '@/pages/Login/template.vue'
-import My from '@/pages/My/template.vue'
-import Register from '@/pages/Register/template.vue'
-import User from '@/pages/user/template.vue'
+// import Index from '@/pages/Index/template.vue'
+// import Create from '@/pages/Create/template.vue'
+// import Detail from '@/pages/Detail/template.vue'
+// import Edit from '@/pages/Edit/template.vue'
+// import Login from '@/pages/Login/template.vue'
+// import My from '@/pages/My/template.vue'
+// import Register from '@/pages/Register/template.vue'
+// import User from '@/pages/user/template.vue'
 
 
 import store from '../store'
 
 Vue.use(Router)
 
+// const router = new Router({
+// 	routes: [
+// 		{
+// 			path: '/',
+// 			component: Index
+// 		},
+// 		{
+// 			path: '/create',
+// 			component: Create,
+// 			meta: { requiresAuth: true }
+// 		},
+// 		{
+// 			path: '/detail/:blogId',
+// 			component: Detail
+// 		},
+// 		{
+// 			path: '/edit/:blogId',
+// 			component: Edit,
+// 			meta: { requiresAuth: true }
+// 		},
+// 		{
+// 			path: '/login',
+// 			component: Login
+// 		},
+// 		{
+// 			path: '/my',
+// 			component: My,
+// 			meta: { requiresAuth: true }
+// 		},
+// 		{
+// 			path: '/register',
+// 			component: Register
+// 		},
+// 		{
+// 			path: '/user/:userId',
+// 			component: User
+// 		}
+// 	]
+// })
+
 const router = new Router({
 	routes: [
 		{
 			path: '/',
-			component: Index
-		},
-		{
-			path: '/create',
-			component: Create,
-			meta: { requiresAuth: true }
-		},
-		{
-			path: '/detail/:blogId',
-			component: Detail
-		},
-		{
-			path: '/edit/:blogId',
-			component: Edit,
-			meta: { requiresAuth: true }
+			component: () => import('@/pages/Index/template.vue')
 		},
 		{
 			path: '/login',
-			component: Login
+			component: () => import('@/pages/Login/template.vue')
+		},
+		{
+			path: '/detail/:blogId',
+			component: () => import('@/pages/Detail/template.vue')
+		},
+		{
+			path: '/edit/:blogId',
+			component: () => import('@/pages/Edit/template.vue'),
+			meta: { requiresAuth: true }
+		},
+		{
+			path: '/create',
+			component: () => import('@/pages/Create/template.vue'),
+			meta: { requiresAuth: true }
+		},
+		{
+			path: '/user/:userId',
+			component: () => import('@/pages/User/template.vue')
 		},
 		{
 			path: '/my',
-			component: My,
+			component: () => import('@/pages/My/template.vue'),
 			meta: { requiresAuth: true }
 		},
 		{
 			path: '/register',
-			component: Register
-		},
-		{
-			path: '/user/:userId',
-			component: User
+			component: () => import('@/pages/Register/template.vue')
 		}
 	]
 })
