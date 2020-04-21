a742c2ffe8319a9cf840715d5139e9440bf3362d
Keller <381796751@qq.com>
2020-04-21 19:19:50 +0800

    引入 vuex

--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+	el: '#app',
+	router,
+	store,
+	components: { App },
+	template: '<App/>'
 })
