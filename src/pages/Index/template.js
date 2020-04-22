5f4162e96006456fd4b746abf6327016f8137613
Keller <381796751@qq.com>
2020-04-22 11:32:58 +0800

    首页完成

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,7 +1,29 @@
+import blog from '@/api/blog.js'
+
 export default {
 	data() {
 		return {
-			msg: '首页'
+			blogs: [],
+			total: 0,
+			page: 1
+		}
+	},
+	created() {
+		this.page = parseInt(this.$route.query.page) || 1
+		blog.getIndexBlogs({ page: this.page }).then(res => {
+			this.blogs = res.data
+			this.total = res.total
+			this.page = res.page
+		})
+	},
+	methods: {
+		onPageChange(newPage) {
+			blog.getIndexBlogs({page: newPage}).then(res => {
+				this.blogs = res.data
+				this.total = res.total
+				this.page = res.page
+				this.$router.push({ path: '/', query: { page: newPage}})
+			})
 		}
 	}
 }
