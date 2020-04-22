fbbced90c19361918428a693eda46b4434651a32
Keller <381796751@qq.com>
2020-04-22 10:42:48 +0800

    完成 创建博客

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -1,7 +1,20 @@
+import blog from '@/api/blog'
+
 export default {
 	data() {
 		return {
-			msg: '创建页面'
+			title: '',
+			description: '',
+			content: '',
+			atIndex: false
+		}
+	},
+	methods: {
+		onCreate() {
+			blog.createBlog({ title: this.title, content: this.content, description: this.description, atIndex: this.atIndex }).then(res => {
+				this.$message.success(res.msg)
+				this.$router.push({ path: `/detail/${res.data.id}` })
+			})
 		}
 	}
 }
