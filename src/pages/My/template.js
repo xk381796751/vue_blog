daef2ce0386a8dbf72430942c28f5850eeb71fda
Keller <381796751@qq.com>
2020-04-22 16:33:35 +0800

    完成 编辑页面

--- src/pages/My/template.js
+++ src/pages/My/template.js
@@ -33,8 +33,8 @@ export default {
 			})
 		},
 
-		async onDelete(blogId) {
-			await this.$confirm('此操作将永久删除该博客, 是否继续?', '提示', {
+		onDelete(blogId) {
+			this.$confirm('此操作将永久删除该博客, 是否继续?', '提示', {
 				confirmButtonText: '确定',
 				cancelButtonText: '取消',
 				type: 'warning'
