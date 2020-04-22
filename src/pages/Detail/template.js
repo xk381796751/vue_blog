57cdd73749ad400a859614fe29053f34c0e1b6d0
Keller <381796751@qq.com>
2020-04-22 12:00:35 +0800

    完成 详情页

--- src/pages/Detail/template.js
+++ src/pages/Detail/template.js
@@ -1,7 +1,23 @@
+import marked from 'marked'
+import blog from '@/api/blog'
+
 export default {
 	data() {
 		return {
-			msg: '详情页面'
+			blogData: null
 		}
+	},
+
+	computed: {
+		markdown() {
+			return marked(this.blogData.content)
+		}
+	},
+
+	created() {
+		const blogId = this.$route.params.blogId
+		blog.getDetail({ blogId }).then(res => {
+			this.blogData = res.data
+		})
 	}
 }
