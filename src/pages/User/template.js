b68096e5d1e91f147f6989b434f057de74a10606
Keller <381796751@qq.com>
2020-04-22 15:01:35 +0800

    完成 user 页面

--- src/pages/User/template.js
+++ src/pages/User/template.js
@@ -1,7 +1,45 @@
+import blog from '@/api/blog'
+
 export default {
 	data() {
 		return {
-			msg: '用户页面'
+			blogs: [],
+			user: {},
+			page: 1,
+			total: 0,
+			userId: this.$route.params.userId
+		}
+	},
+
+	created() {
+		this.page = parseInt(this.$route.query.page) || 1
+		blog.getBlogsByUserId(this.userId, { page: this.page }).then(res => {
+			this.page = res.page
+        	this.total = res.total
+			this.blogs = res.data
+			if (res.data.length > 0) {
+				this.user = res.data[0].user
+			}
+		})
+	},
+
+	methods: {
+		onPageChange(newPage) { 
+			blog.getBlogsByUserId(this.userId, { page: newPage }).then(res => {
+				this.page = res.page
+				this.total = res.total
+				this.blogs = res.data
+				this.$router.push({ path: `/user/${this.userId}`, query: { page: newPage}})
+			})
+		},
+
+		splitDate(dataStr) {
+			let dateObj = typeof dataStr === 'object' ? dataStr : new Date(dataStr)
+			return {
+				date: dateObj.getDate(),
+				month: dateObj.getMonth() + 1,
+				year: dateObj.getFullYear()
+			}
 		}
 	}
 }
